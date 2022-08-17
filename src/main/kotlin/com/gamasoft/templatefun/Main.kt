a5f39bd93c4f9683d1d6569661ddbdacffcd4e22
Uberto Barbini <uberto.gama@gmail.com>
2022-08-17 15:21:41 +0900

    testing results

--- src/main/kotlin/com/gamasoft/templatefun/Main.kt
+++ src/main/kotlin/com/gamasoft/templatefun/Main.kt
@@ -1,8 +1,14 @@
 package com.gamasoft.templatefun
 
-class Main {
+import java.lang.ArithmeticException
 
+    fun test(a: Int): Result<Int> = Result.success(a)
+    fun test2(a: Int): Result<Int> = Result.failure(ArithmeticException("Not a valid number $a"))
     fun main(){
-        println("Hello")
+
+        val x = test(345)
+        println("success $x")
+
+        val y = test2(4)
+        println("failure $y")
     }
-}
